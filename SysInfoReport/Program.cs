7662ed9b40316c8e8e587082d3ea00bf4a9a4e18
nafanity <nafanity@dozor.biz>
2016-11-04 14:11:57 +0300

    Добавлено несколько функций

--- SysInfoReport/Program.cs
+++ SysInfoReport/Program.cs
@@ -8,8 +8,13 @@ namespace SysInfoReport
         {
             //get parametrs in string format
             String ProccessorName = SystemInformation.getCPUInfo();
+            String RAM = SystemInformation.getRAMInfo();
+            String HDD = SystemInformation.getHDDInfo();
+            String Motherboard = SystemInformation.getMotherBoardInfo();
+            String VideoCard = SystemInformation.getVideoBoardInfo();
+            String Monitor = SystemInformation.getMonitorInfo();
 
-            Console.WriteLine(ProccessorName);
+            Console.WriteLine(Monitor);
             Console.ReadLine();
             
 
--- SysInfoReport/SystemInformation.cs
+++ SysInfoReport/SystemInformation.cs
@@ -28,5 +28,130 @@ namespace SysInfoReport
 
             return result;
         }
+
+        static public String getRAMInfo()
+        {
+            var query = "SELECT * FROM Win32_PhysicalMemory";
+            String result = "";
+            String separator = "; ";
+
+            int Count = 0; //Количество планок
+            long TotalMemory = 0;
+            String frequency = "";
+
+            ManagementObjectSearcher searcher =
+                new ManagementObjectSearcher(path, query);
+
+            foreach (var queryObj in searcher.Get())
+            {
+                Count++;
+                TotalMemory += Convert.ToInt64(queryObj["Capacity"]) / 1024 / 1024;
+                frequency = queryObj["Speed"].ToString();
+
+            }
+
+            result = TotalMemory.ToString() + " Mb" +
+                    separator +
+                    frequency + " Mhz" +
+                    separator + Count.ToString();
+
+            return result;
+        }
+
+        static public String getHDDInfo()
+        {
+            var query = "SELECT * FROM Win32_DiskPartition  ";
+            String result = "Total size: ";
+            String partOfDisk = "(";
+            double totalSpace = 0;
+            
+
+            ManagementObjectSearcher searcher =
+                new ManagementObjectSearcher(path, query);
+
+            foreach (var queryObj in searcher.Get())
+            {
+                double sizePartOfDisk = Math.Round(Convert.ToDouble(queryObj["Size"]) / 1024 / 1024 / 1024, 2);
+                totalSpace += sizePartOfDisk;
+                partOfDisk += sizePartOfDisk.ToString();
+                partOfDisk += " GB ";
+            }
+
+            partOfDisk += ")";
+
+            result += totalSpace.ToString() + " GB:" + partOfDisk;
+
+            return result;   
+        }
+
+        static public String getMotherBoardInfo()
+        {
+            var query = "SELECT * FROM Win32_BaseBoard ";
+            String result = "";
+
+
+            ManagementObjectSearcher searcher =
+                new ManagementObjectSearcher(path, query);
+
+            foreach (var queryObj in searcher.Get())
+            {
+                result += queryObj["Manufacturer"];
+                result += queryObj["Product"];
+            }
+
+
+            return result;
+        }
+
+        static public String getVideoBoardInfo()
+        {
+            var query = "SELECT * FROM Win32_VideoController  ";
+            String result = "";
+            bool flagNewString = false;
+
+
+            ManagementObjectSearcher searcher =
+                new ManagementObjectSearcher(path, query);
+
+            foreach (var queryObj in searcher.Get())
+            {
+                if (flagNewString)
+                    result += "\n";
+                result += queryObj["Name"];
+                result += " ";
+                result += Math.Round(Convert.ToDouble(queryObj["AdapterRAM"]) / 1024 / 1024, 2);
+                result += " MB ";
+                flagNewString = true;
+            }
+
+            return result;
+        }
+
+        static public String getMonitorInfo()
+        {
+            var query = "SELECT * FROM Win32_DesktopMonitor   ";
+            String result = "";
+            bool flagNewString = false;
+
+
+            ManagementObjectSearcher searcher =
+                new ManagementObjectSearcher(path, query);
+
+            foreach (var queryObj in searcher.Get())
+            {
+                if (queryObj["Name"].ToString() == "Универсальный монитор PnP")
+                    continue;
+                if (flagNewString)
+                    result += "\n";
+                result += queryObj["Name"];
+                result += " ";
+                result += Convert.ToInt32(queryObj["ScreenWidth"]).ToString();
+                result += "x";
+                result += Convert.ToInt32(queryObj["ScreenHeight"]).ToString();
+                flagNewString = true;
+            }
+
+            return result;
+        }
     }
 }
